4c5b51d9712cbfb5f7e9b6e902c50b910cf47dc4
Joachim Hofer <jmhofer.github@johoop.de>
2012-02-22 09:42:58 +0100

    added Sonatype publish settings

--- project/Build.scala
+++ project/Build.scala
@@ -13,24 +13,54 @@ object TestNGPluginBuild extends Build {
         "org.scala-tools.testing" % "test-interface" % "0.5" % "provided",
         "org.testng" % "testng" % "6.4" % "provided",
         "com.google.inject" % "guice" % "2.0" % "provided")))
-  
+
   lazy val testNGPlugin = Project(
     id = "sbt-testng-plugin",
     base = file("plugin"),
     settings = Project.defaultSettings ++ commonSettings ++ Seq(
       sbtPlugin := true,
       version := "2.0.0-SNAPSHOT"))
-  
+
   override def settings = super.settings ++ Seq(EclipseKeys.skipParents in ThisBuild := false)
-  
-  lazy val commonSettings: Seq[Setting[_]] = Seq(
+
+  lazy val commonSettings: Seq[Setting[_]] = publishSettings ++ Seq(
     organization := "de.johoop",
     scalaVersion := "2.9.1",
     publishArtifact in (Compile, packageDoc) := false,
-    publishTo := Some("Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/releases/"),
-    credentials += Credentials(Path.userHome / ".ivy2" / ".credentials"),
     scalacOptions ++= Seq("-unchecked", "-deprecation"))
+
+  lazy val publishSettings: Seq[Setting[_]] = Seq(
+    publishTo <<= version { v: String =>
+      val nexus = "https://oss.sonatype.org/"
+      if (v.trim.endsWith("SNAPSHOT")) Some("snapshots" at nexus + "content/repositories/snapshots")
+      else Some("releases" at nexus + "service/local/staging/deploy/maven2")
+    },
+    publishMavenStyle := true,
+    publishArtifact in Test := false,
+    pomIncludeRepository := { x => false },
+    pomExtra := (
+      <url>https://www.bitbucket.org/jmhofer/sbt-testng-interface</url>
+      <licenses>
+        <license>
+          <name>Eclipse Public License v1.0</name>
+          <url>http://www.eclipse.org/legal/epl-v10.html</url>
+          <distribution>repo</distribution>
+        </license>
+      </licenses>
+      <scm>
+        <url>https://www.bitbucket.org/jmhofer/sbt-testng-interface</url>
+        <connection>scm:hg:https://www.bitbucket.org/jmhofer/sbt-testng-interface</connection>
+      </scm>
+      <developers>
+        <developer>
+          <id>johofer</id>
+          <name>Joachim Hofer</name>
+          <url>http://jmhofer.johoop.de</url>
+        </developer>
+        <developer>
+          <id>asflierl</id>
+          <name>Andreas Flierl</name>
+          <url>http://flierl.eu</url>
+        </developer>
+      </developers>))
 }
-
-
-
