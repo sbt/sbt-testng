15ac0e51c0a953f2bdb7ca69294c16666201886e
Joachim Hofer <jmhofer.github@johoop.de>
2012-12-20 12:10:01 +0100

    updated to scala 2.10.0 final

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,7 @@ object TestNGPluginBuild extends Build {
     base = file("."),
     settings = Project.defaultSettings ++ commonSettings ++ Seq(
       version := "2.0.3",
-      crossScalaVersions := Seq("2.8.1", "2.9.0", "2.9.1", "2.9.2", "2.10.0-RC1"),
+      crossScalaVersions := Seq("2.8.1", "2.9.0", "2.9.1", "2.9.2", "2.10.0"),
       libraryDependencies ++= Seq(
         "org.scala-tools.testing" % "test-interface" % "0.5" % "provided",
         "org.testng" % "testng" % "6.7" % "provided",
@@ -20,13 +20,14 @@ object TestNGPluginBuild extends Build {
     settings = Project.defaultSettings ++ commonSettings ++ Seq(
       sbtPlugin := true,
       version := "2.0.3",
-      crossScalaVersions := Seq("2.9.2")))
+      crossScalaVersions := Seq("2.9.2", "2.10.0")))
 
   override def settings = super.settings ++ Seq(EclipseKeys.skipParents in ThisBuild := false)
 
   lazy val commonSettings: Seq[Setting[_]] = publishSettings ++ Seq(
     organization := "de.johoop",
-    scalaVersion := "2.9.2",
+    scalaVersion := "2.10.0",
+    resolvers += "Sonatype Release" at "https://oss.sonatype.org/content/repositories/releases",
     scalacOptions ++= Seq("-unchecked", "-deprecation"))
 
   lazy val publishSettings: Seq[Setting[_]] = Seq(
