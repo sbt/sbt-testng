a88e168e546ea1cd1d058e9dec643eb30b1990f3
Joachim Hofer <jmhofer.github@johoop.de>
2013-10-20 11:32:44 +0200

    moved to github sbt org

--- project/Build.scala
+++ project/Build.scala
@@ -1,6 +1,5 @@
 import sbt._
 import sbt.Keys._
-import com.typesafe.sbteclipse.plugin.EclipsePlugin.EclipseKeys
 
 object TestNGPluginBuild extends Build {
   lazy val root = Project(
@@ -8,7 +7,7 @@ object TestNGPluginBuild extends Build {
     base = file("."),
     settings = Project.defaultSettings ++ commonSettings ++ Seq(
       version := "3.0.0",
-      crossScalaVersions := Seq("2.9.3", "2.10.2"),
+      crossScalaVersions := Seq("2.9.3", "2.10.3"),
       libraryDependencies ++= Seq(
         "org.scala-sbt" % "test-interface" % "1.0" % "provided",
         "org.testng" % "testng" % "6.8.5" % "provided",
@@ -20,48 +19,19 @@ object TestNGPluginBuild extends Build {
     settings = Project.defaultSettings ++ commonSettings ++ Seq(
       sbtPlugin := true,
       version := "3.0.0",
-      crossScalaVersions := Seq("2.10.2"),
+      crossScalaVersions := Seq("2.10.3"),
       scalacOptions += "-language:_"))
-
-  override def settings = super.settings ++ Seq(EclipseKeys.skipParents in ThisBuild := false)
 
   lazy val commonSettings: Seq[Setting[_]] = publishSettings ++ Seq(
     organization := "de.johoop",
-    scalaVersion := "2.10.2",
+    scalaVersion := "2.10.3",
     scalacOptions ++= Seq("-unchecked", "-deprecation"))
 
   lazy val publishSettings: Seq[Setting[_]] = Seq(
-    publishTo := {
-      val nexus = "https://oss.sonatype.org/"
-      if (version.value.trim.endsWith("SNAPSHOT")) Some("snapshots" at nexus + "content/repositories/snapshots")
-      else Some("releases" at nexus + "service/local/staging/deploy/maven2")
+    publishTo := { 
+      val qualifier = "sbt-plugin-" + (if (version.value contains "-SNAPSHOT") "snapshots" else "releases")
+      Some(Resolver.url(qualifier, new URL(s"http://repo.scala-sbt.org/scalasbt/$qualifier"))(Resolver.ivyStylePatterns))
     },
-    publishMavenStyle := true,
-    publishArtifact in Test := false,
-    pomIncludeRepository := { x => false },
-    pomExtra := (
-      <url>https://www.bitbucket.org/jmhofer/sbt-testng-interface</url>
-      <licenses>
-        <license>
-          <name>Eclipse Public License v1.0</name>
-          <url>http://www.eclipse.org/legal/epl-v10.html</url>
-          <distribution>repo</distribution>
-        </license>
-      </licenses>
-      <scm>
-        <url>https://www.bitbucket.org/jmhofer/sbt-testng-interface</url>
-        <connection>scm:hg:https://www.bitbucket.org/jmhofer/sbt-testng-interface</connection>
-      </scm>
-      <developers>
-        <developer>
-          <id>johofer</id>
-          <name>Joachim Hofer</name>
-          <url>http://jmhofer.johoop.de</url>
-        </developer>
-        <developer>
-          <id>asflierl</id>
-          <name>Andreas Flierl</name>
-          <url>http://flierl.eu</url>
-        </developer>
-      </developers>))
+    publishMavenStyle := false,
+    publishArtifact in Test := false)
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,3 +1,2 @@
 addSbtPlugin("com.typesafe.sbt" % "sbt-pgp" % "0.8")
 
-addSbtPlugin("com.typesafe.sbteclipse" % "sbteclipse-plugin" % "2.3.0")
