ca730f705f48af2139f39bc726b474afec072738
Joachim Hofer <jmhofer.github@johoop.de>
2014-09-06 21:41:07 +0200

    attempt to move to bintray publishing

--- project/Build.scala
+++ project/Build.scala
@@ -1,34 +1,36 @@
 import sbt._
 import sbt.Keys._
+import bintray.Plugin._
+import bintray.Keys._
 
 object TestNGPluginBuild extends Build {
-  lazy val root = Project(
-    id = "sbt-testng-interface",
-    base = file("."),
-    settings = Project.defaultSettings ++ commonSettings ++ Seq(
+  lazy val root = Project(id = "sbt-testng-interface", base = file("."))
+    .settings(commonSettings: _*)
+    .settings(
       version := "3.0.2",
-      crossScalaVersions := Seq("2.9.3", "2.10.4", "2.11.1"),
+      crossScalaVersions := Seq("2.9.3", "2.10.4", "2.11.2"),
       libraryDependencies ++= Seq(
         "org.scala-sbt" % "test-interface" % "1.0" % "provided",
         "org.testng" % "testng" % "6.8.8" % "provided",
-        "com.google.inject" % "guice" % "2.0" % "provided")))
+        "com.google.inject" % "guice" % "2.0" % "provided"))
 
-  lazy val testNGPlugin = Project(
-    id = "sbt-testng-plugin",
-    base = file("plugin"),
-    settings = Project.defaultSettings ++ commonSettings ++ Seq(
+  lazy val testNGPlugin = Project(id = "sbt-testng-plugin", base = file("plugin"))
+    .settings(commonSettings: _*)
+    .settings(
       sbtPlugin := true,
       version := "3.0.2",
       crossScalaVersions := Seq("2.10.4"),
-      scalacOptions += "-language:_"))
+      scalacOptions += "-language:_")
 
   lazy val commonSettings: Seq[Setting[_]] = publishSettings ++ Seq(
     organization := "de.johoop",
     scalaVersion := "2.10.4",
     scalacOptions ++= Seq("-unchecked", "-deprecation"))
 
-  lazy val publishSettings: Seq[Setting[_]] = Seq(
-    publishTo := Some(Resolver.sbtPluginRepo(if (isSnapshot.value) "snapshots" else "releases")),
+  lazy val publishSettings: Seq[Setting[_]] = bintrayPublishSettings ++ Seq(
     publishMavenStyle := false,
+    licenses += ("BSD", url("http://opensource.org/licenses/BSD-3-Clause")),
+    repository in bintray := "sbt-plugins",
+    bintrayOrganization in bintray := None,
     publishArtifact in Test := false)
 }
